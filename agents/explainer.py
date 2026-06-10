e30972d25724122ff298b04365614840143d68f9
kamalesh2602 <kamaleshg2602@gmail.com>
2026-06-10 13:13:09 +0530

    fixed prompts to be in english

--- agents/explainer.py
+++ agents/explainer.py
@@ -1,8 +1,25 @@
 from config import model
-
+from langchain_core.messages import HumanMessage, SystemMessage
 def explain_topic(topic:str):
     response = model.invoke(
-        f"Explain {topic} for a beginner."
+        def generate_quiz(topic: str):
+    return quiz_model.invoke(
+        [
+            SystemMessage(
+                content="""
+                You are an English quiz generator.
+
+                Always generate responses in English.
+                """
+            ),
+            HumanMessage(
+                content=f"""
+                Generate one quiz question
+                about {topic}
+                """
+            )
+        ]
+    )
     )
 
     return response.content
--- agents/quiz.py
+++ agents/quiz.py
@@ -1,11 +1,25 @@
 from config import model
 from schemas.quiz import QuizQuestion
-
+from langchain_core.messages import SystemMessage, HumanMessage
 quiz_model = model.with_structured_output(
     QuizQuestion
 )
 
 def generate_quiz(topic: str):
     return quiz_model.invoke(
-        f"Generate one quiz question about {topic}"
+        [
+            SystemMessage(
+                content="""
+                You are an English quiz generator.
+
+                Always generate responses in English.
+                """
+            ),
+            HumanMessage(
+                content=f"""
+                Generate one quiz question
+                about {topic}
+                """
+            )
+        ]
     )
