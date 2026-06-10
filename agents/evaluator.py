127bade0ec555e3568e646e13b70f233b057e184
kamalesh2602 <kamaleshg2602@gmail.com>
2026-06-10 13:08:28 +0530

    evaluator agent

---
+++ agents/evaluator.py
@@ -0,0 +1,22 @@
+from config import model
+from schemas.evaluation import Evaluation
+
+evaluation_model = model.with_structured_output(
+    Evaluation
+)
+
+def evaluate_answer(
+    question: str,
+    answer: str
+):
+    return evaluation_model.invoke(
+        f"""
+        Question:
+        {question}
+
+        Student Answer:
+        {answer}
+
+        Evaluate it.
+        """
+    )
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from agents.explainer import explain_topic
 from agents.quiz import generate_quiz
+from agents.evaluator import evaluate_answer
+
 
 result = explain_topic("Python Integers")
 print(result)
@@ -9,3 +11,13 @@ quiz = generate_quiz("Python Lists")
 print(quiz)
 print(quiz.question)
 print(quiz.answer)
+
+
+result = evaluate_answer(
+    "What is OOP?",
+    "OOP is Object Oriented Programming"
+)
+
+print(result.score)
+print(result.feedback)
+print(result.improvement_area)
